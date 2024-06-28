54cce6472640ec898b49f02c79cc1b237f2fc577
Rushikesh Koli <163403477+rushikesh-koli-11@users.noreply.github.com>
2024-06-28 19:40:57 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  plugins: [react()],
+  define: {
+    // By default, Vite doesn't include shims for NodeJS/
+    // necessary for segment analytics lib to work
+    global: {},
+  },
   base: '/',
   build: {
     outDir: 'dist',
   },
 });
+
+
